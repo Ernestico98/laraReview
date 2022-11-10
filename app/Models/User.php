489ff76607ad71eb1dd63f1beeb94537a6a957b7
Ernestico98 <ernestodavidph@gmail.com>
2022-11-10 21:10:01 +0100

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function places()
+    {
+        return $this->hasMany(Place::class, 'author_id');
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class, 'author_id');
+    }
 }
