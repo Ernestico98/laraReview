20c43b6c1a7eead0371cacccb56943ba568e3a7e
Ernestico98 <ernestodavidph@gmail.com>
2022-11-10 21:00:59 +0100

    add Place model

---
+++ app/Models/Place.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Place extends Model
+{
+    use HasFactory;
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->using(PlaceTag::class);
+    }
+}
---
+++ database/factories/PlaceFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Place>
+ */
+class PlaceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->realTextBetween(10, 13),
+            'description' => fake()->realTextBetween(20, 30),
+            'author_id' => fake()->numberBetween(1, \App\Models\User::count()),
+        ];
+    }
+}
