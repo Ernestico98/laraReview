406f03a86ca19b57772b091e6dca6303adc13edf
Ernestico98 <ernestodavidph@gmail.com>
2022-11-10 21:02:28 +0100

    add Review model

---
+++ app/Models/Review.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    public function place()
+    {
+        return $this->belongsTo(Place::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'author_id' => fake()->numberBetween(1, \App\Models\User::count()),
+            'place_id' => fake()->numberBetween(1, \App\Models\Place::count()),
+            'score' => fake()->numberBetween(1, 5),
+            'review' => fake()->realText(),
+        ];
+    }
+}
