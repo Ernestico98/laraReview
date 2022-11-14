9b77f9b7037662dd2f2e69c968ea1dea324edcd3
Ernestico98 <ernestodavidph@gmail.com>
2022-11-15 00:40:26 +0100

    name home route

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
