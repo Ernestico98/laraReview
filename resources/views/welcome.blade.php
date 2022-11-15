804df4b0155f48ca916e33a913fd0ac9bbddf84a
Ernestico98 <ernestodavidph@gmail.com>
2022-11-15 09:08:10 +0100

    add breeze to project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-site-layout>
+    Hola
+</x-site-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
