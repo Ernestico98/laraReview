70066a620f237a9f771a4281cd52dabf29b0364a
Ernestico98 <ernestodavidph@gmail.com>
2022-11-16 00:52:47 +0100

    add button to create new place in places.create

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -1,6 +1,13 @@
 <x-site-layout text="Places">
+
+    <div class="mx-20 mb-4 flex flex-row justify-end">
+        <a href="{{route('places.create')}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+            Add Place
+        </a>
+    </div>
+
     @foreach ($places as $place)
-        <div class="mx-20 my-12 bg-gray-50 rounded-lg flex flex-row shadow-lg shadow-gray-200 max-h-60">
+        <div class="mx-20 mb-12 bg-gray-50 rounded-lg flex flex-row shadow-lg shadow-gray-200 max-h-60">
             <div class="basis-1/4 rounded-lg pb-10 overflow-hidden">
                 <img src="https://www.idsplus.net/wp-content/uploads/default-placeholder.png">
             </div>
