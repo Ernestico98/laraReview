c301cf732a49584049c7a00c810c6c04b7a53653
Ernestico98 <ernestodavidph@gmail.com>
2022-11-17 00:43:35 +0100

    wip fix component parameters

--- resources/views/components/form-textarea-input.blade.php
+++ resources/views/components/form-textarea-input.blade.php
@@ -1,5 +1,5 @@
 <div class="flex place-items-center mt-6">
     <label for="{{$name}}" class="w-24 text-sm text-gray-500">{{$label}}</label>
-    <textarea name="{{$name}}" placeholder="{{$placeholder}}" class="w-[{{$width}}px] h-{{$height}} border border-gray-400 rounded-sm p-1">{{old($name, "")}}</textarea>
+    <textarea name="{{$name}}" placeholder="{{$placeholder}}" class="w-[{{$width}}px] h-{{$height}} border border-gray-400 rounded-sm p-1">{{old($name, $value)}}</textarea>
 </div>
 <x-form-error name="{{$name}}"/>
--- resources/views/places/create.blade.php
+++ resources/views/places/create.blade.php
@@ -2,13 +2,13 @@
 
     <div class="mx-8 my-6">
         <x-form method="post" route="{{route('places.store')}}" submit="Create">
-            <x-form-text-input name="name" label="Place Name" placeholder="Name of the place" />
+            <x-form-text-input name="name" label="Place Name" placeholder="Name of the place" value=""/>
 
-            <x-form-text-input name="city" label="City" placeholder="Place's city" />
+            <x-form-text-input name="city" label="City" placeholder="Place's city" value=""/>
 
-            <x-form-textarea-input name="description" placeholder="Write a short description of the place" label="Description" width="500" height="20"/>
+            <x-form-textarea-input name="description" placeholder="Write a short description of the place" label="Description" width="500" height="20" value=""/>
 
-            <x-form-textarea-input name="tags" placeholder="Write tags separated by comma and no extra spaces" label="Tags" width="250" height="16"/>
+            <x-form-textarea-input name="tags" placeholder="Write tags separated by comma and no extra spaces" label="Tags" width="250" height="16" value=""/>
 
             <div class="flex place-items-center mt-6">
                 <label class="w-24 text-sm text-gray-500">TODO: Add upload photo</label>
