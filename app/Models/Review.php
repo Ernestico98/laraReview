4561aaca702da8c801dd2a8ccb6d2a169396b55f
Ernestico98 <ernestodavidph@gmail.com>
2022-11-19 18:11:29 +0100

    set parameters fillable an add onCreate method to Review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,8 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     public function place()
     {
         return $this->belongsTo(Place::class);
@@ -17,5 +19,19 @@ class Review extends Model
     public function author()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($review) {
+            $place = Place::findOrFail($review->place_id);
+
+            $count = $place->reviews->count();
+            $new_score = round((($place->score * ($count - 1) + $review->score) / $count), 1);
+
+            $place->update([
+                'score' => $new_score,
+            ]);
+        });
     }
 }
