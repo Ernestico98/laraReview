0b72cda25c1b833772496c78f5bac185490c4df5
Ernestico98 <ernestodavidph@gmail.com>
2022-11-21 00:07:24 +0100

    add welcome notification when user created

---
+++ app/Notifications/WelcomeUserNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeUserNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Welcome to LaraRe')
+            ->greeting('Hello '.$notifiable->name.',')
+            ->line('Welcome to LaraRe')
+            ->line('where you will find reviews for all your favourite place')
+            ->action('Get Started', route('home'))
+            ->line('Thank you for being part of our community')
+            ->salutation('Greetings from the LaraRe team!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
