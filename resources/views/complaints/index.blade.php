ca38b37e0d1a7d79226b58422032ddc025bdfcb4
Ernestico98 <ernestodavidph@gmail.com>
2022-11-21 00:44:39 +0100

    add pagination to tags.index

--- resources/views/complaints/index.blade.php
+++ resources/views/complaints/index.blade.php
@@ -1,7 +1,17 @@
 <x-site-layout text="Reported reviews">
+
+    <div class="mx-6">
+        {{$reviews->links()}}
+    </div>
+
     <div class="mx-6">
         @foreach ($reviews as $review)
         <x-review-reported-card :review="$review" />
         @endforeach
     </div>
+
+    <div class="mx-6">
+        {{$reviews->links()}}
+    </div>
+
 </x-site-layout>
