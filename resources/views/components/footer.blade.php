a400672dd1a2b4cd8dddc563d675f96f4afc5143
Ernestico98 <ernestodavidph@gmail.com>
2022-11-22 17:12:41 +0100

    remove admin section from footer (not needed)

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,9 @@
     <div class="container px-6 pt-6 mx-auto">
       <div class="flex justify-center mb-6">
         @foreach ($menu as $key => $item)
-        <a href="{{$item['url']}}" class="text-white px-3 py-2 rounded-md text-base font-semibold" aria-current="page">{{$item['title']}}</a>
+            @if($item['title'] != 'Administration')
+                <a href="{{$item['url']}}" class="text-white px-3 py-2 rounded-md text-base font-semibold" aria-current="page">{{$item['title']}}</a>
+            @endif
         @endforeach
       </div>
     </div>
