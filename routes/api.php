8080826a2e5c547bca0f782ccd55f27dff4ce2c3
Ernestico98 <ernestodavidph@gmail.com>
2023-01-23 20:24:47 +0100

    add apiResource Controller to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::get('places/index', [\App\Http\Controllers\Api\PlaceController::class, 'index'])->name('api_place.index');
+// Route::get('places/show/{id}', [\App\Http\Controllers\Api\PlaceController::class, 'show'])->name('api_place.show');
+
+
+// Api version 1 routes
+Route::middleware(['auth:sanctum', 'ApiIsAdmin'])->prefix('v1')->group( function() {
+
+    Route::apiResource('places', \App\Http\Controllers\Api\PlaceController::class);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
